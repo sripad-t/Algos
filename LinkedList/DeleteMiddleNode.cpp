5ac9ed69f3bb8e3ca83348a91f63a96c95b0118e
sripad-tvss <63554431+sripad-tvss@users.noreply.github.com>
2022-10-15 19:51:52 +0530

    Create DeleteMiddleNode.cpp

---
+++ LinkedList/DeleteMiddleNode.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head -> next == nullptr) return nullptr;
+        ListNode* fast = head, *slow = head, *prev = nullptr;
+        while(fast && fast -> next != nullptr) {
+            fast = fast -> next -> next;
+            prev = slow;
+            slow = slow -> next;
+        }
+        prev -> next = prev -> next -> next;
+        return head;
+    }
+};
